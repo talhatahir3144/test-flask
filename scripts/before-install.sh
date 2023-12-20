ff71b04ee733038acb1b44d9b1cf82568ad8602f
Muhammad Talha Tahir <talha.tahir@systemsltd.com>
2023-12-21 00:16:09 +0500

    updated application-start.sh

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Set executable permissions for specific scripts
+chmod +x /var/www/flask-app/scripts/after-install.sh
+chmod +x /var/www/flask-app/scripts/application-start.sh
+chmod +x /var/www/flask-app/scripts/application-stop.sh
+
 sudo apt-get update
 sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
