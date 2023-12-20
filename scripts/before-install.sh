0d3c02cb6215807f586ec672d66044c5b5b9c136
Muhammad Talha Tahir <talha.tahir@systemsltd.com>
2023-12-21 00:30:08 +0500

    updated application-start.sh

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+# Any error during execution causes the script to exit
+set -e
 
 # Set executable permissions for specific scripts
 chmod +x /var/www/flask-app/scripts/after-install.sh
 chmod +x /var/www/flask-app/scripts/application-start.sh
 chmod +x /var/www/flask-app/scripts/application-stop.sh
 
+# Stop the existing application server
+sudo systemctl stop myapp.service || echo "Application server could not be stopped or was not running."
+
+# Clean up any existing files in the application directory
+sudo rm -rf /var/www/flask-app/*
+# Installing Python
 sudo apt-get update
 sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
