8e6f9d034fcdf71da241ea50345732342de3cf39
Muhammad Talha Tahir <talhatahir586@gmail.com>
2023-12-21 06:15:41 -0500

    Update application-stop.sh

--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -1,16 +1,17 @@
+# # #!/bin/bash
+
+# # # Read the PID from the file
+# # pid=$(cat /var/run/flask-app.pid)
+
+# # # Kill the process
+# # kill $pid
+
+# # # Remove the PID file
 # # rm /var/run/flask-app.pid
 
 # #Name of the application, adjust as per your Python script's name
-# APP_NAME="app.py"
 PID_5000_PORT=$(sudo lsof -i :5000 | grep LISTEN | awk '{print $2}')
 
 # #Use pgrep to find the process ID of your Flask app based on its name
-# PID=$(pgrep -f $APP_NAME)
-
-# #If the application is running, kill it
-# if [ ! -z "$PID" ]; then
-#     echo "Stopping Flask app"
-#     kill $PID
-# fi
 echo "Stopping Flask app"
 kill $PID_5000_PORT
