3810ce292c63614daa2afcc18c2c4f71adcaa006
Muhammad Talha Tahir <talhatahir586@gmail.com>
2023-12-21 05:57:00 -0500

    Update application-stop.sh

--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -10,8 +10,17 @@
 # # rm /var/run/flask-app.pid
 
 # #Name of the application, adjust as per your Python script's name
-PID_5000_PORT=$(sudo lsof -i :5000 | grep LISTEN | awk '{print $2}')
 
 # #Use pgrep to find the process ID of your Flask app based on its name
-echo "Stopping Flask app"
-kill -SIGTERM $PID_5000_PORT
+#echo "Stopping Flask app"
+#!/bin/bash
+
+# Find the process using port 5000 and kill it
+#PID=$(sudo lsof -t -i:5000)
+PID=$(sudo lsof -i :5000 | grep LISTEN | awk '{print $2}')
+if [ -z "$PID" ]; then
+    echo "No process is using port 5000."
+else
+    echo "Killing process on port 5000 with PID: $PID"
+    sudo kill $PID
+fi
